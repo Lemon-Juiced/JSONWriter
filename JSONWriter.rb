91ef958f928730c1bcea26b3bf2832c5e8223f19
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2024-09-25 19:33:06 -0400

    Fixed comment

--- JSONWriter.rb
+++ JSONWriter.rb
@@ -1,6 +1,6 @@
 =begin
 JSONWriter is an application that takes a command line argument containing a namespace, the name of a resource, and a -i, -t, or -c flag denoting an item or tag (either in the given or common directory).
-It then reads the resource and writes the data to several JSON files.
+It then reads the arguments and writes the data to several JSON files.
 These JSON files are for Minecraft recipes, for a set of tools and armor.
 The JSON files are written to the current directory.
 
